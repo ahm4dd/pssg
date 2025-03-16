0f8fbbf269b85bf9eddf3feb3b1ea69472045613
ahm4dd <ahmed.emad.edu@gmail.com>
2025-03-16 14:37:54 +0300

    Fixed conditionals in to_html() in leafnode and added the incomplete version of parentnode.py

--- src/leafnode.py
+++ src/leafnode.py
@@ -5,11 +5,11 @@ class LeafNode(HTMLNode):
         super().__init__(tag, value, props=props)
 
     def to_html(self):
-        if self.value == "":
+        if self.value is None or self.value == "":
             raise ValueError("All leaf nodes must have a value.")
-        if self.tag is None:
+        if self.tag is None or self.tag == "":
             return self.value
-        if self.props is None:
+        if self.props is None or self.props == {}:
             return f"<{self.tag}>{self.value}</{self.tag}>" 
         return f"<{self.tag} {super().props_to_html()}>{self.value}</{self.tag}>"
     
---
+++ src/parentnode.py
@@ -0,0 +1,10 @@
+from htmlnode import *
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag=tag, children=children, props=props)
+
+    def to_html(self):
+        if self.tag == "":
+            pass
+        pass
