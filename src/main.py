d95217ac2158da1b779cfea2df8a5f79b6c9e3fe
ahm4dd <ahmed.emad.edu@gmail.com>
2025-03-20 23:31:19 +0300

    Improved the copy_to_public function to check for subfiles and subdirectories and added an images directory

--- src/main.py
+++ src/main.py
@@ -7,20 +7,35 @@ from parentnode import *
 from parser import *
 from converter import *
 
+IMAGES_EXTENSIONS = ['png', 'jpg', 'svg', 'webg']
+
 def main():
     copy_to_public()
 
-def copy_to_public():
+def copy_to_public(path='static/'):
+    files = os.listdir(path)
+    if path =='static/':
+        if os.listdir("public/") != []:
+            shutil.rmtree('public/')
+            os.mkdir('public/')
+            os.mkdir('public/images')
+
     if not(os.path.exists("static/")):
         os.mkdir('static/') 
     if not(os.path.exists("public/")):
         os.mkdir('public/')
+    if not(os.path.exists("public/images")):
+        os.mkdir('public/images')
 
-    if os.listdir("public/") != []:
-        shutil.rmtree('public/')
-        os.mkdir('public/')
-    if os.listdir('static/') != []:
-        for file in os.listdir("static/"):
-            if os.path.isfile(os.path.join('static/', file)):
-                shutil.copy(os.path.join('static/', file), 'public/') 
+    
+    if files != []:
+        for file in files:
+            if '.' not in file:
+                copy_to_public(path+file+'/')
+            if os.path.isfile(os.path.join(path, file)):
+                print(file)
+                if file.split('.')[1] in IMAGES_EXTENSIONS:
+                    shutil.copy(os.path.join(path, file), 'public/images')
+                else:
+                    shutil.copy(os.path.join(path, file), 'public/')
 main()
