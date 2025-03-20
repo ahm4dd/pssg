3fd27696b90dddaa8a3f5e8bb528e7c84895686c
ahm4dd <ahmed.emad.edu@gmail.com>
2025-03-20 22:54:40 +0300

    Fixed the logic errors with the copy_to_public function, it checks if the directories exist first and then executes

--- src/main.py
+++ src/main.py
@@ -11,12 +11,16 @@ def main():
     copy_to_public()
 
 def copy_to_public():
-    if os.path.exists("static/"):
-        if os.path.exists("public/"):
-          if os.listdir("public/") != []:
-              shutil.rmtree('public/')
-              os.mkdir('public/')
-          for file in os.listdir("static/"):
-              shutil.copy(os.path.join('static/', file), 'public/')
+    if not(os.path.exists("static/")):
+        os.mkdir('static/') 
+    if not(os.path.exists("public/")):
+        os.mkdir('public/')
 
+    if os.listdir("public/") != []:
+        shutil.rmtree('public/')
+        os.mkdir('public/')
+    if os.listdir('static/') != []:
+        for file in os.listdir("static/"):
+            if os.path.isfile(os.path.join('static/', file)):
+                shutil.copy(os.path.join('static/', file), 'public/') 
 main()
