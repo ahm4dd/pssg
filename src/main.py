399c5647c3cfa5e8fb6fe67fdff3e6d4ae75fac1
ahm4dd <ahmed.emad.edu@gmail.com>
2025-03-28 05:32:33 +0300

    Hosting the website on github pages

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import sys
 from textnode import *
 from htmlnode import *
 from leafnode import *
@@ -8,26 +9,30 @@ from parser import *
 from converter import *
 
 IMAGES_EXTENSIONS = ['png', 'jpg', 'jpeg', 'svg']
+default_basepath = "/"
 
 def main():
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
     copy_to_public()
-    generate_pages_recursive('content/','template.html','public/')
+    generate_pages_recursive('content/','template.html','docs/', basepath)
     
 
 def copy_to_public(path='static/'):
     files = os.listdir(path)
     if path =='static/':
-        if os.listdir("public/") != []:
-            shutil.rmtree('public/')
-            os.mkdir('public/')
-            os.mkdir('public/images')
+        if os.listdir("docs/") != []:
+            shutil.rmtree('docs/')
+            os.mkdir('docs/')
+            os.mkdir('docs/images')
 
     if not(os.path.exists("static/")):
         os.mkdir('static/') 
-    if not(os.path.exists("public/")):
-        os.mkdir('public/')
-    if not(os.path.exists("public/images")):
-        os.mkdir('public/images')
+    if not(os.path.exists("docs/")):
+        os.mkdir('docs/')
+    if not(os.path.exists("docs/images")):
+        os.mkdir('docs/images')
 
     
     if files != []:
@@ -37,11 +42,11 @@ def copy_to_public(path='static/'):
             if os.path.isfile(os.path.join(path, file)):
                 print(file)
                 if file.split('.')[1] in IMAGES_EXTENSIONS:
-                    shutil.copy(os.path.join(path, file), 'public/images')
+                    shutil.copy(os.path.join(path, file), 'docs/images')
                 else:
-                    shutil.copy(os.path.join(path, file), 'public/')
+                    shutil.copy(os.path.join(path, file), 'docs/')
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f'Generating page from {from_path} to {dest_path} using {template_path}')
 
     try:
@@ -52,7 +57,8 @@ def generate_page(from_path, template_path, dest_path):
         page_title = extract_title(markdown)
         template = template.replace('{{ Title }}', page_title)
         template = template.replace('{{ Content }}', html_from_markdown)
-
+        template = template.replace('href="/', 'href="' + basepath)
+        template = template.replace('src="/', 'src="' + basepath)
         if not(os.path.exists(os.path.dirname(dest_path))):
             os.makedirs(os.path.dirname(dest_path))
         if not(os.path.exists(dest_path)):
@@ -63,7 +69,7 @@ def generate_page(from_path, template_path, dest_path):
             f.close()
     except Exception as e:
         print(e)
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     entries = os.listdir(dir_path_content)
     
     for entry in entries:
@@ -74,7 +80,7 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
             if not os.path.exists(new_dest_dir):
                 os.makedirs(new_dest_dir)
             
-            generate_pages_recursive(entry_path + '/', template_path, new_dest_dir + '/')
+            generate_pages_recursive(entry_path + '/', template_path, new_dest_dir + '/', basepath)
         
         elif entry.endswith('.md'):
             if entry == 'index.md':
@@ -82,5 +88,5 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
             else:
                 dest_file_path = os.path.join(dest_dir_path, entry.replace('.md', '.html'))
             
-            generate_page(entry_path, template_path, dest_file_path)
+            generate_page(entry_path, template_path, dest_file_path, basepath)
 main()
